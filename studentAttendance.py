d84372a6f6b6a7b3cb29b91acfb09e96f03d48c0
goutham <gouthammsd475@gmail.com>
2026-06-11 23:59:13 +0530

    Added database

--- studentAttendance.py
+++ studentAttendance.py
@@ -2,6 +2,13 @@ from pymongo import MongoClient
 import pandas as pd
 from datetime import datetime
 
+client = MongoClient("mongodb://localhost:27017")
+db = client["attendance_db"]
+
+students = db["students_record"]
+attendance_record = db["attendance"]
+
+
 def add_student():
     sid = int(input("enter the student ID:"))
     name = input("enter the student name :")
@@ -14,19 +21,22 @@ def mark_attendance():
     sid = int(input("enter the student ID:"))
     attendance = input("enter the attendance (present/absent):")
 
-    if sid in students:
-        if attendance == present:
-            attendance_record.insert_one({"sid": sid, "attendance": attendance, "timestamp": datetime.now()})
-        else:
-            attendance_record.insert_one({"sid":sid,"attendance": attendance,"timestamp":datetime.now()})
-            print("attendance marked successfully")
+    student = students.find_one({"sid": sid})
+    if attendance == "present":
+        attendance_record.insert_one({"sid": sid, "attendance": attendance, "timestamp": datetime.now()})
+    elif attendance == "absent":
+        attendance_record.insert_one({"sid":sid,"attendance": attendance,"timestamp":datetime.now()})
+        print("attendance marked successfully")
     else:
         print("student not found")
 
-choice = int(input("enter the number to select options:\n1.add student\n2.mark attendance\n"))
-if choice ==1:
-    add_student()
-elif choice == 2:
-    mark_attendance()
-else:
-    print("invalid choice:select correct option")
+
+
+while True:
+    choice = int(input("enter the number to select options:\n1.add student\n2.mark attendance\n"))
+    if choice ==1:
+        add_student()
+    elif choice == 2:
+        mark_attendance()
+    else:
+        print("invalid choice:select correct option")
