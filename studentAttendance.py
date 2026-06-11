1b66252d83ca9cf2a206a213e40c266224eb734f
goutham <gouthammsd475@gmail.com>
2026-06-12 01:15:09 +0530

    features added

--- studentAttendance.py
+++ studentAttendance.py
@@ -12,6 +12,9 @@ attendance_record = db["attendance"]
 def add_student():
     sid = int(input("enter the student ID:"))
     name = input("enter the student name :")
+    if students.find_one({"sid": sid}):
+        print("student already exists")
+        return
 
     students.insert_one({"sid": sid, "name": name})
 
@@ -30,13 +33,29 @@ def mark_attendance():
     else:
         print("student not found")
 
+def view_attendance():
+    for student in students.find():
+        print(student)
 
+def view_specific_student_attendance():
+    sid = int(input("enter the student ID:"))
+    student = students.find_one({"sid":sid})
+    if student:
+        attendance = attendance_record.find_one({"sid":sid})
+        print(attendance)
+    else:
+        print("student not found")
 
 while True:
-    choice = int(input("enter the number to select options:\n1.add student\n2.mark attendance\n"))
+    choice = int(input("enter the number to select options:\n1.add student\n2.mark attendance\n3.view attendance\n4.view specific student attendance\n"))
     if choice ==1:
         add_student()
     elif choice == 2:
         mark_attendance()
+    elif choice == 3:
+        view_attendance()
+    elif choice == 4:
+        view_specific_student_attendance()
     else:
         print("invalid choice:select correct option")
+        break
